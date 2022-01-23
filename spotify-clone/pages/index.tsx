e076251652fb308fc33c2c060ba566ef62ad6c9e
AkshayBenny <akshaybennygh@gmail.com>
2022-01-23 11:45:45 +0530

    starting files set up

---
+++ spotify-clone/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Head>
+        <title>Spotify Clone</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <h1>Hello world</h1>
+    </div>
+  );
+}
