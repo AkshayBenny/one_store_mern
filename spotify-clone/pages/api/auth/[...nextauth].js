59a68791a878f209c8c95a8a93a044e6f0c160dd
AkshayBenny <akshaybennygh@gmail.com>
2022-01-24 17:16:16 +0530

    authentication added

--- spotify-clone/pages/api/auth/[...nextauth].js
+++ spotify-clone/pages/api/auth/[...nextauth].js
@@ -1,13 +1,40 @@
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import SpotifyProvider from 'next-auth/providers/spotify';
+import { LOGIN_URL } from '../../../lib/spotify';
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    SpotifyProvider({
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: './login',
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      // Initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_ar * 1000,
+        };
+      }
+
+      //Return previous token if the access token is still valid
+      if (Date.now() < token.accessTokenExpires){
+          return token
+      }
+
+      //Access token has expired, so we need to refresh it...after: 
+    },
+  },
 });
